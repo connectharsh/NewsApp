9e9b7bdc37ff2fc14bd6f04f595a6e398d0832ce
harshdeep <harshdeepprofessional@gmail.com>
2023-02-07 19:02:54 +0530

    added Infinity scroll and removed buttons from the app

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,22 +1,23 @@
-import React , {Component} from 'react'
-export class MyCard extends Component{
-    render(){
-        let {title,description,image} = this.props;
-    return(
-        <>
-        <div className="col-md-4 col-sm-10" id='myCard'>
-            <div className="originalCard">
-                <img src={image?image:'https://thumbs.dreamstime.com/b/sample-stamp-white-background-sign-90532936.jpg'} alt="imgtxt"></img>
-                <h2>{title?title:"Sample title for kingNewz".slice(0,40)}.....</h2>
-                <div id="paragraph">
-                     <p>{description?description:'Sorry unable to fetch description from News api because No description is available for this news or this news has been deleted please refresh this page for more info '.slice(0,300)}.....</p>
-                     <p id="readMore"><a href="/">Read More...</a></p>
-                </div>   
-            </div>
-            
-        </div>
-        </>
-    );
+import React, { Component } from 'react'
+export class MyCard extends Component {
+    render() {
+        let { title, description, image, date, author } = this.props;
+        return (
+            <>
+                <div className="col-md-4 col-sm-12" id='myCard'>
+                    <div className="originalCard">
+                        <img src={image ? image : 'https://thumbs.dreamstime.com/b/sample-stamp-white-background-sign-90532936.jpg'} alt="imgtxt"></img>
+                        <h2>{title ? title.slice(0, 40) : "Sample title for kingNewz"}.....</h2>
+                        <div id="paragraph">
+                            <p>{description ? description.slice(0, 180) : 'Sorry unable to fetch description from News api because No description is available for this news or this news has been deleted please refresh this page for more info '}.....</p>
+                            <p id="readMore"><a href="/">Read More...</a></p>
+                        </div>
+                        <p className="card-text" id="userInfoDiv"><small className="text-muted" id='userInfo'>Last updated on {date ? date : "XX-XX-XXXX"} by {author ? author : "unknown"}</small></p>
+                    </div>
+
+                </div>
+            </>
+        );
     }
 }
 export default MyCard;
--- src/components/News.js
+++ src/components/News.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react'
 import MyCard from './MyCard';
 import Spinner from './Spinner';
+import InfiniteScroll from "react-infinite-scroll-component";
 import PropTypes from 'prop-types'
 
 export class News extends Component {
   static defaultProps = {
-    pageSize:9,
-    country:'in',
-    category:'general',
+    pageSize: 9,
+    country: 'in',
+    category: 'general',
+    currSize: 1,
   }
   static propTypes = {
     pageSize: PropTypes.number,
     country: PropTypes.string,
-    category:PropTypes.string,
+    category: PropTypes.string,
   }
   // adding a single default item to our articles array.
   articles = [
-   
+
   ]
 
   // setting the stae by using constructor
@@ -25,62 +27,55 @@ export class News extends Component {
     this.state = {
       articles: this.articles,
       page: 1,
+      totalResults:0,
       loading: true,
     }
   }
 
-//this function runs after adding the dom
+  //this function runs after adding the dom
   async componentDidMount() {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.countryName}&category=${this.props.category}&apiKey=364e8e98229640c68ba584320bb61b7b&pageSize=${this.props.pageSize}`;
     let data = await fetch(url)
     data = await data.json()
-    this.setState({ articles: data.articles,totalResults:data.totalResults,loading:false, });
-    
+    this.setState({ articles: data.articles, totalResults: data.totalResults, loading: false, });
   }
 
-  //this handle the previous click and change the url page number 
-  handlePreviousClick = async()=> {
-    this.setState(
-      {loading:true,}
-    )
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.countryName}&category=${this.props.category}&apiKey=364e8e98229640c68ba584320bb61b7b&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+  fetchData = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.countryName}&category=${this.props.category}&apiKey=364e8e98229640c68ba584320bb61b7b&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url)
     data = await data.json()
-    this.setState({ articles: data.articles,
-    page: this.state.page-1, 
-    loading:false,})
-
-    console.log('previous clicked');
-  }
-  // handling the next click 
-  handleNextClick= async()=> {
-    this.setState(
-      {loading:true,}
-    )
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.countryName}&category=${this.props.category}&apiKey=364e8e98229640c68ba584320bb61b7b&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-    let data = await fetch(url)
-    data = await data.json()
-    this.setState({ articles: data.articles,
-    page: this.state.page+1, 
-    loading:false,})
-    console.log('next clicked');
+    this.setState({
+      articles: this.state.articles.concat(data.articles),
+      page: this.state.page + 1,
+    })
   }
 
   //simple rendering the files
   render() {
     return (
       <>
-        {this.state.loading && <Spinner/>}
-        {<h1 id='mainHeading'>{this.state.loading?'loading':'Top Headlines'}</h1>}
-        <div className="myContainer">
-          {!this.state.loading && this.state.articles.map((element) => {
-            return <MyCard key={element.url} title={element.title} description={element.description} image={element.urlToImage} />
-          })}
-        </div>
-        {!this.state.loading && <div id="buttons">
-          <button className="button btn btn-primary mx-2 my-2" disabled = {this.state.page<=1} onClick={this.handlePreviousClick}>&#8249;</button>
-          <button className="button btn btn-primary mx-2 my-2" disabled = {Math.ceil(this.state.totalResults/this.props.pageSize)===this.state.page}onClick={this.handleNextClick}>&#8250;</button>
-        </div>}
+        {<h1 id='mainHeading'>{this.state.loading ? 'loading': 'Top Headlines'}</h1>}
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length} //This is important field to render the next data
+          next={this.fetchData}
+          hasMore={this.state.articles.length!== this.state.totalResults}
+          loader={<Spinner/>}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>This is the end of the page toalResults, = {this.state.totalResults}</b>
+            </p>
+
+          }>
+          <div className="myContainer">
+            {!this.state.loading && this.state.articles.map((element) => {
+              let publishedAT = new Date(element.publishedAt);
+              publishedAT = `${publishedAT.getDate()}/${publishedAT.getMonth()}/${publishedAT.getFullYear()}`
+              return <MyCard key={element.url} title={element.title} description={element.description} image={element.urlToImage} date={publishedAT} author={element.author} />
+            })}
+
+          </div>
+        </InfiniteScroll>
       </>
     )
   }
